939ffb245019bab1a2ad80466fd779497442cd02
hexagon-recursion <hexagon-recursion@posteo.net>
2023-11-25 19:57:55 +0300

    Add naive encode/decode

--- main.lua
+++ main.lua
@@ -7,7 +7,32 @@ local M = require 'posix.sys.socket'
 
 -- Thanks https://stackoverflow.com/a/65477617/
 function hex(str)
-	return str:gsub(".", function(char) return string.format("%02x", char:byte()) end)
+	return str:gsub(".", function(char) return string.format("%02x", char:byte()) end):gsub("........", "%1 ")
+end
+
+function encode(object_id, opcode, payload)
+	-- TODO: check: is object_id in wayland packet header unsigned?
+	-- TODO: check: is opcode in wayland packet header unsigned?
+	-- TODO: check: does lua work fine with 32 bit unsigned integers?
+	local format = "I4I4c" .. payload:len()
+	local op_and_len = string.packsize(format) << 16 | opcode 
+	return string.pack(format, object_id, op_and_len, payload) 
+end
+
+function decode(message)
+	local format = "I4I4"
+	if message:len() < string.packsize(format) then
+		print("no header") -- DEBUG
+		return nil
+	end
+	local object_id, op_and_len = string.unpack(format, message)
+	local len = op_and_len >> 16
+	local opcode = op_and_len & (1 << 16 - 1)
+	if message:len() < len then
+		print("too small") -- DEBUG
+		return nil
+	end
+	return object_id, opcode, message:sub(9, len)
 end
 
 function ping_the_server()
@@ -19,9 +44,12 @@ function ping_the_server()
 	print("socket() -> " .. s)
 	print("connect() -> " .. assert(M.connect(s, {family = M.AF_UNIX, path = socket_path})) )
 	-- TODO: this assumes "host's byte-order" is little-endian. How to detect byte order in lua?
-        print("send() -> " .. assert(M.send(s, "\x01\x00\x00\x00\x00\x00\x0c\x00\x02\x00\x00\x00")))
-	print("message: ", hex(assert(M.recv(s, 12))))
-	print("message: ", hex(assert(M.recv(s, 12))))
+	local wl_display_sync = encode(1, 0, "\x02\x00\x00\x00")
+        print("send() -> " .. assert(M.send(s, wl_display_sync)))
+	local i, o, p = decode(assert(M.recv(s, 12)))
+	print(string.format("got event: object %d opcode %d %q", i, o, hex(p)))
+	local i, o, p = decode(assert(M.recv(s, 12)))
+	print(string.format("got event: object %d opcode %d %q", i, o, hex(p)))
 end
 
 ping_the_server()
