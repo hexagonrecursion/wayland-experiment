0c567bf0fea968fe3be6fbbffed6926ef7f3fbfd
hexagon-recursion <hexagon-recursion@posteo.net>
2023-11-26 08:45:41 +0300

    parttially implement decoder

--- main.lua
+++ main.lua
@@ -7,7 +7,8 @@ local M = require 'posix.sys.socket'
 
 -- Thanks https://stackoverflow.com/a/65477617/
 function hex(str)
-	return str:gsub(".", function(char) return string.format("%02x", char:byte()) end):gsub("........", "%1 ")
+	local result = str:gsub(".", function(char) return string.format("%02x", char:byte()) end):gsub("........", "%1 ")
+	return result
 end
 
 wl_display = {}
@@ -20,6 +21,7 @@ function wl_display.new(o)
 end
 
 function wl_display:sync()
+	-- TODO: create a wl_callback class?
 	local callback = {}
 	return {self.id, 0, "new_id", callback}, callback
 end
@@ -41,6 +43,20 @@ end
 
 -- Append `bytes` to buffer, return an array of events
 function Wayland:from_server(bytes)
+	self.bytes_from_server = self.bytes_from_server .. bytes
+	local format = "I4I4"
+	events = {}
+	while true do
+		if self.bytes_from_server:len() < string.packsize(format) then break end
+		local object_id, op_and_len = string.unpack(format, self.bytes_from_server)
+		local len = op_and_len >> 16
+		local opcode = op_and_len & (1 << 16 - 1)
+		if self.bytes_from_server:len() < len then break end
+		table.insert(events, {object_id, opcode, self.bytes_from_server:sub(9, len)})
+		-- TODO: optimize
+		self.bytes_from_server = self.bytes_from_server:sub(len + 1)
+	end
+	return events
 end
 
 -- Encode an array of requests
@@ -58,7 +74,9 @@ function Wayland:to_server(requests)
 	for i = 3, #req, 2 do
 		if req[i] == "new_id" then
 			req[i+1].id = self.next_free_id
+			self.object_by_id[req[i+1].id] = req[i+1]
 			payload = payload .. string.pack("I4", req[i+1].id)
+			self.next_free_id = 1 + self.next_free_id
 		else
 			assert(nil, "unknown type "..req[i])
 		end
@@ -100,11 +118,22 @@ function ping_the_server()
 	local request = display:sync()
 	local bytes = wayland:to_server({request})
         print("send() -> " .. assert(M.send(socket, bytes)))
-
-	local i, o, p = decode(assert(M.recv(socket, 12)))
-	print(string.format("got event: object %d opcode %d %q", i, o, hex(p)))
-	local i, o, p = decode(assert(M.recv(socket, 12)))
-	print(string.format("got event: object %d opcode %d %q", i, o, hex(p)))
+	
+	while true do
+		-- TODO: decide buffer size
+		-- TODO: is wayland a steram protocol or datagram protocol?
+		bytes = assert(M.recv(socket, 2 << 16))
+		for _, event in ipairs(wayland:from_server(bytes)) do
+			for _, v in ipairs(event) do
+				if type(v) == "string" then
+					print(hex(v))
+				else
+					print(v)
+				end
+			end
+			print("-------------")
+		end
+	end
 end
 
 ping_the_server()
